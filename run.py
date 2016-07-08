43b99321616d9d8fb788c9d8eb3c568c29ea674c
Lasote <lasote@gmail.com>
2016-07-08 23:50:06 +0200

    Separador no al fondo

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def margin_05(measure):
     return [mes - MARGIN for mes in measure]
 
 DEEP = 56
-WIDTH = 133
+WIDTH = 130
 HEIGHT = 238
 
 FLOOR_MEASURES = (0, 0)
@@ -25,18 +25,16 @@ print("---------------------------------\n")
 print("Tablero techo: %s" % str(margin_05((WIDTH, DEEP))))
 print("Tablero suelo: %s" % str(margin_05(FLOOR_MEASURES)))
 
-print("Tapa trasera maletero: %s" % str(margin_05((WIDTH, TRUNK_HEIGHT))))
-print("Tapas laterales maletero 2x: %s" % str(margin_05((DEEP - WRAPPER_WOOD_WIDTH, TRUNK_HEIGHT))))
+print("Tapa trasera maletero: %s" % str((WIDTH - MARGIN, TRUNK_HEIGHT)))
+print("Tapas laterales maletero 2x: %s" % str((DEEP - WRAPPER_WOOD_WIDTH - MARGIN, TRUNK_HEIGHT)))
 
 ancho_izquierda_abajo = LEFT_FRAME + NEEDED_GAP_LEFT
-ancho_derecha_abajo = WIDTH - (LEFT_FRAME + NEEDED_GAP_LEFT + INTERIOR_WOOD_WIDTH)
+ancho_derecha_abajo = WIDTH - ancho_izquierda_abajo
 
-alto_abajo = HEIGHT - (2*WRAPPER_WOOD_WIDTH) - TRUNK_HEIGHT - INTERIOR_WOOD_WIDTH 
+alto_abajo = HEIGHT - (2 * WRAPPER_WOOD_WIDTH) - TRUNK_HEIGHT - INTERIOR_WOOD_WIDTH
 
 print("Tapa fondo abajo 1: %s" % str((ancho_izquierda_abajo, alto_abajo)))
 print("Tapa fondo abajo 2: %s" % str((ancho_derecha_abajo, alto_abajo)))
-
-
 print("Tapas lateral abajo 2x: %s" % str((GENERAL_DEEP - MARGIN - WRAPPER_WOOD_WIDTH, alto_abajo)))
 
 
@@ -46,8 +44,4 @@ print("Tablero maletero: %s" % str((WIDTH - MARGIN, GENERAL_DEEP)))
 print("Separador vertical: %s" % str((GENERAL_DEEP, alto_abajo)))
 print("Baldas izquierda 2x: %s" % str((ancho_izquierda_abajo, GENERAL_DEEP)))
 print("Baldas derecha 1x: %s" % str((ancho_derecha_abajo, GENERAL_DEEP)))
-
 print("\n")
-
-
-
